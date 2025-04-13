6a4f6bdccb5bafddeab1339d1018b6c4a7b5380e
Никита <nikitval@list.ru>
2025-04-13 12:49:52 +0300

    book catalog init

--- src/pages/BookCatalogPage/BookCatalogPage.jsx
+++ src/pages/BookCatalogPage/BookCatalogPage.jsx
@@ -3,7 +3,14 @@ import styles from './BookCatalogPage.module.css'
 export const BookCatalogPage = (props) => {
     return (
         <div className={styles.wrapper}>
-            каталог
+            <div className={styles.container}>
+                <div className={styles.filters}>
+                    filters
+                </div>
+                <div className={styles.bookList}>
+                    books
+                </div>
+            </div>
         </div>
     )
 }
